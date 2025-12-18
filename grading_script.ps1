e264bda465bc3674a1b56770b2f72693db3db0bf
Aron Larsson <arlars03@gmail.com>
2025-12-18 17:14:29 +0100

    Remove unnecessary env variables

--- grading_script.ps1
+++ grading_script.ps1
@@ -3,17 +3,18 @@ param(
     [int]$taskNumber
 )
 
-$env:TDA357_GRADING_ROOT = (Get-Item $PSScriptRoot).FullName
-$env:TDA357_UTIL_ROOT = (Join-Path $env:TDA357_GRADING_ROOT "util")
-$env:TDA357_TASKS_ROOT = (Join-Path $env:TDA357_GRADING_ROOT "Tasks")
+$gradingRoot = (Get-Item $PSScriptRoot).FullName
+$env:TDA357_UTIL_ROOT = (Join-Path $gradingRoot "util")
+$tasksRoot = (Join-Path $gradingRoot "Tasks")
+$env:TDA357_TASK_NUMBER = $taskNumber
 
-$groupFolderName = Invoke-Expression ".\util\submission_extraction.ps1 -TaskNumber $taskNumber"
+$groupFolderName = Invoke-Expression "$env:TDA357_UTIL_ROOT\submission_extraction.ps1"
 if (-not $groupFolderName) {
     Write-Host "Failed to extract and move submissions."
     exit
 }
 
-$env:TDA357_GROUP_SUBMISSION_ROOT = Join-Path $env:TDA357_TASKS_ROOT "Task$taskNumber" "student_submission" $groupFolderName
-$env:TDA357_TASK_ROOT = Join-Path $env:TDA357_TASKS_ROOT "Task$taskNumber"
+$env:TDA357_TASK_ROOT = Join-Path $tasksRoot "Task$taskNumber"
+$env:TDA357_GROUP_SUBMISSION_ROOT = Join-Path $env:TDA357_TASK_ROOT "student_submission" $groupFolderName
 
 Invoke-Expression "$env:TDA357_UTIL_ROOT\t${taskNumber}_script.ps1"
