62a3b4b231cb9f6fddda429798ba4b1966f54588
Aron Larsson <arlars03@gmail.com>
2025-12-18 17:43:10 +0100

    Move server part into t4 script

--- util/t4_script.ps1
+++ util/t4_script.ps1
@@ -8,19 +8,21 @@ if ((-not $env:TDA357_GROUP_SUBMISSION_ROOT) -or -not (Test-Path $env:TDA357_GRO
     exit
 }
 
-Copy-Item -Path "$env:TDA357_TASK_ROOT\initial\runsetup.sql" -Destination (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "runsetup.sql") -ErrorAction SilentlyContinue
-Invoke-Expression "psql -f '$env:TDA357_GROUP_SUBMISSION_ROOT\runsetup.sql' 'postgresql://postgres:postgres@127.0.0.1'" | Out-Null
+Read-Host ("Check the following before trying to run the server:`n" + 
+    "- Database config is correct(username/password/dbname)`n" + 
+    "- There is no package declaration in the files`n" + 
+    "Press enter to continue")
 
-# Show diffs for .txt and .sql files between the groups two submissions
-if (Test-Path (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT 'OLD')) {
-    Get-ChildItem $env:TDA357_GROUP_SUBMISSION_ROOT | Where-Object Name -match '^*\.(txt|sql)' | ForEach-Object {
-    Write-Host ''
-    $viewDiffInput = Read-Host "Press enter to view $($_.Name) diff (s to skip)"
-    if ($viewDiffInput -eq 's') { 
-        return 
-    }
-    code --wait --diff (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT 'OLD' $_.Name) (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT $_.Name)
-    }
+if (Test-Path (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT 'PortalConnection.py')) {
+    Copy-Item -Path "$env:TDA357_TASK_ROOT\initial\python\PortalServer.py" -Destination (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "PortalServer.py") -ErrorAction SilentlyContinue
+    $process = Start-Process -PassThru pwsh "-NoExit -Command $env:TDA357_GRADING_ROOT\venv\Scripts\python.exe '$env:TDA357_GROUP_SUBMISSION_ROOT\PortalServer.py'"
+} elseif (Test-Path (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT 'PortalConnection.java')) {
+    Copy-Item -Path "$env:TDA357_TASK_ROOT\initial\java\PortalServer.java" -Destination (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "PortalServer.java") -ErrorAction SilentlyContinue
+    $cp = "$env:TDA357_GROUP_SUBMISSION_ROOT;$env:TDA357_TASK_ROOT\initial\java\postgresql-42.7.4.jar"
+    javac -cp $cp (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "PortalConnection.java")
+    javac -cp $cp (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "PortalServer.java")
+    javac -cp $cp (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "TestPortal.java")
+    $process = Start-Process -PassThru pwsh "-NoExit -Command java -cp '$cp' PortalServer"
 }
 
 Read-Host "Press enter to terminate the server"
