dbcd4f99c9c7965b3d8e24a8c19e7d07bcbb76ad
Aron Larsson <arlars03@gmail.com>
2025-12-18 18:14:09 +0100

    Fix better failure message

--- util/t2_script.ps1
+++ util/t2_script.ps1
@@ -1,10 +1,10 @@
 if ((-not $env:TDA357_TASK_ROOT) -or -not (Test-Path $env:TDA357_TASK_ROOT)) {
-    Write-Host "TDA357_TASK_ROOT is not set or does not exist."
+    Write-Host "TDA357_TASK_ROOT is not set or directory does not exist."
     exit
 }
 
 if ((-not $env:TDA357_GROUP_SUBMISSION_ROOT) -or -not (Test-Path $env:TDA357_GROUP_SUBMISSION_ROOT)) {
-    Write-Host "TDA357_GROUP_SUBMISSION_ROOT is not set or does not exist."
+    Write-Host "TDA357_GROUP_SUBMISSION_ROOT is not set or directory does not exist."
     exit
 }
 
