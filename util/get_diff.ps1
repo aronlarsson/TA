68f75b1308e7a7ea2079b982ab0319c633758ddb
Aron Larsson <arlars03@gmail.com>
2025-12-19 17:40:43 +0100

    Include java and python files in the diff

--- util/get_diff.ps1
+++ util/get_diff.ps1
@@ -5,7 +5,7 @@ if ((-not $env:TDA357_GROUP_SUBMISSION_ROOT) -or -not (Test-Path $env:TDA357_GRO
 
 # Show diffs for .txt and .sql files between the groups two submissions
 if (Test-Path (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT 'OLD')) {
-    Get-ChildItem $env:TDA357_GROUP_SUBMISSION_ROOT | Where-Object Name -match '^*\.(txt|sql)' | ForEach-Object {
+    Get-ChildItem $env:TDA357_GROUP_SUBMISSION_ROOT | Where-Object Name -match '^*\.(txt|sql|java|py)' | ForEach-Object {
     Write-Host ''
     $viewDiffInput = Read-Host "Press enter to view $($_.Name) diff (s to skip)"
     if ($viewDiffInput -eq 's') { 
