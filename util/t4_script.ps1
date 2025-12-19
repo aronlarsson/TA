de8434b33ba3f73eb510dae491df354c3b5c5584
Aron Larsson <arlars03@gmail.com>
2025-12-19 19:29:33 +0100

    Fix: run setup before starting server

--- util/t4_script.ps1
+++ util/t4_script.ps1
@@ -18,6 +18,8 @@ Read-Host ("Check the following before trying to run the server:`n" +
     "- There is no package declaration in the files`n" + 
     "Press enter to continue")
 
+Invoke-Expression "psql -f '$env:TDA357_GROUP_SUBMISSION_ROOT\runsetup.sql' 'postgresql://postgres:postgres@127.0.0.1'" | Out-Null
+
 if (Test-Path (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT 'PortalConnection.py')) {
     Copy-Item -Path "$env:TDA357_TASK_ROOT\initial\python\PortalServer.py" -Destination (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "PortalServer.py") -ErrorAction SilentlyContinue
     $process = Start-Process -PassThru pwsh "-NoExit -Command $env:TDA357_GRADING_ROOT\venv\Scripts\python.exe '$env:TDA357_GROUP_SUBMISSION_ROOT\PortalServer.py'"
