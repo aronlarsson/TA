343129e7cb2cd813a970039c691ea8eb5d6f4db6
Aron Larsson <arlars03@gmail.com>
2025-12-19 17:40:24 +0100

    Fix: test correct path and add test for task root path

--- grading_script.ps1
+++ grading_script.ps1
@@ -6,8 +6,8 @@ param(
 $env:TDA357_GRADING_ROOT = (Get-Item $PSScriptRoot).FullName
 
 $env:TDA357_UTIL_ROOT = (Join-Path $env:TDA357_GRADING_ROOT "util")
-if (-not (Test-Path $env:TDA357_TASK_NUMBER)) {
-   New-Item -ItemType Directory -Path $env:TDA357_TASK_NUMBER | Out-Null
+if (-not (Test-Path $env:TDA357_UTIL_ROOT)) {
+   New-Item -ItemType Directory -Path $env:TDA357_UTIL_ROOT | Out-Null
 }
 
 $tasksRoot = (Join-Path $env:TDA357_GRADING_ROOT "Tasks")
@@ -17,13 +17,17 @@ if (-not (Test-Path $tasksRoot)) {
 
 $env:TDA357_TASK_NUMBER = $taskNumber
 
+$env:TDA357_TASK_ROOT = Join-Path $tasksRoot "Task$taskNumber"
+if (-not (Test-Path $env:TDA357_TASK_ROOT)) {
+   New-Item -ItemType Directory -Path $env:TDA357_TASK_ROOT | Out-Null
+}
+
 $groupFolderName = Invoke-Expression "$env:TDA357_UTIL_ROOT\submission_extraction.ps1"
 if (-not $groupFolderName) {
     Write-Host "Failed to extract and move submissions."
     exit
 }
 
-$env:TDA357_TASK_ROOT = Join-Path $tasksRoot "Task$taskNumber"
 $env:TDA357_GROUP_SUBMISSION_ROOT = Join-Path $env:TDA357_TASK_ROOT "student_submission" $groupFolderName
 
 Invoke-Expression "$env:TDA357_UTIL_ROOT\t${taskNumber}_script.ps1"
