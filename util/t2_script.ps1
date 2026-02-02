41fdadd8fdcfa215ddeabe0acc121d5bcf921fe2
Aron Larsson <arlars03@gmail.com>
2026-02-02 08:28:30 +0100

    Add files from db-lp2-ht25

---
+++ util/t2_script.ps1
@@ -0,0 +1,16 @@
+if ((-not $env:TDA357_TASK_ROOT) -or -not (Test-Path $env:TDA357_TASK_ROOT)) {
+    Write-Host "TDA357_TASK_ROOT is not set or directory does not exist."
+    exit
+}
+
+if ((-not $env:TDA357_GROUP_SUBMISSION_ROOT) -or -not (Test-Path $env:TDA357_GROUP_SUBMISSION_ROOT)) {
+    Write-Host "TDA357_GROUP_SUBMISSION_ROOT is not set or directory does not exist."
+    exit
+}
+
+# Open the ER diagram and the solution
+$erDiagramPath = Join-Path "$env:TDA357_GROUP_SUBMISSION_ROOT" "ER.png"
+Start-Process $erDiagramPath
+$erDiagramSolutionPath = Join-Path $env:TDA357_TASK_ROOT "solutions\ER.png"
+$erDiagramSolutionUri = [uri]::EscapeDataString($erDiagramSolutionPath)
+Start-Process "chrome" "$erDiagramSolutionUri"
---
+++ util/t4_script.ps1
@@ -0,0 +1,40 @@
+if ((-not $env:TDA357_TASK_ROOT) -or -not (Test-Path $env:TDA357_TASK_ROOT)) {
+    Write-Host "TDA357_TASK_ROOT is not set or directory does not exist."
+    exit
+}
+
+if ((-not $env:TDA357_GROUP_SUBMISSION_ROOT) -or -not (Test-Path $env:TDA357_GROUP_SUBMISSION_ROOT)) {
+    Write-Host "TDA357_GROUP_SUBMISSION_ROOT is not set or directory does not exist."
+    exit
+}
+
+if ((-not $env:TDA357_GRADING_ROOT) -or -not (Test-Path $env:TDA357_GRADING_ROOT)) {
+    Write-Host "TDA357_GRADING_ROOT is not set or directory does not exist."
+    exit
+}
+
+Read-Host ("Check the following before trying to run the server:`n" + 
+    "- Database config is correct(username/password/dbname)`n" + 
+    "- There is no package declaration in the files`n" + 
+    "Press enter to continue")
+
+Invoke-Expression "psql -f '$env:TDA357_GROUP_SUBMISSION_ROOT\runsetup.sql' 'postgresql://postgres:postgres@127.0.0.1'" | Out-Null
+
+if (Test-Path (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT 'PortalConnection.py')) {
+    Copy-Item -Path "$env:TDA357_TASK_ROOT\initial\python\PortalServer.py" -Destination (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "PortalServer.py") -ErrorAction SilentlyContinue
+    $process = Start-Process -PassThru pwsh "-NoExit -Command $env:TDA357_GRADING_ROOT\venv\Scripts\python.exe '$env:TDA357_GROUP_SUBMISSION_ROOT\PortalServer.py'"
+} elseif (Test-Path (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT 'PortalConnection.java')) {
+    Copy-Item -Path "$env:TDA357_TASK_ROOT\initial\java\PortalServer.java" -Destination (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "PortalServer.java") -ErrorAction SilentlyContinue
+    $cp = "$env:TDA357_GROUP_SUBMISSION_ROOT;$env:TDA357_TASK_ROOT\initial\java\postgresql-42.7.4.jar"
+    javac -cp $cp (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "PortalConnection.java")
+    javac -cp $cp (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "PortalServer.java")
+    javac -cp $cp (Join-Path $env:TDA357_GROUP_SUBMISSION_ROOT "TestPortal.java")
+    $process = Start-Process -PassThru pwsh "-NoExit -Command java -cp '$cp' PortalServer"
+}
+
+Read-Host "Press enter to terminate the server"
+Get-CimInstance win32_process -Filter "ParentProcessId = $($process.Id)" | ForEach-Object {
+    Write-Host "Terminating child process Id: $($_.ProcessId)"
+    Stop-Process $_.ProcessId -Force
+}
+Stop-Process $process -Force
